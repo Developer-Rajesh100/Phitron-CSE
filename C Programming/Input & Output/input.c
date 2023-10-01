bbc7354547cce6c3184ae04cd6cac90a357601c3
Developer-Rajesh100 <rajeshhabra100@gmail.com>
2023-10-01 22:42:18 +0530

    First Commit

---
+++ C Programming/Input & Output/input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+
+    printf("Enter Any Intiger Number : ");
+    scanf("%d", &number);
+
+    printf("Your Entered Number is : %d", number);
+
+    return 0;
+}
---
+++ C Programming/Variable/variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int number = 100;
+    float num2 = 5.367;
+
+    printf("%d\n", number);
+
+    printf("%0.2f\n", num2);
+
+    return 0;
+}
