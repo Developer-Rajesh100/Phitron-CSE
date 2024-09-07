ac2fe6ec1395c7163756c37a63232c09f3c2550f
Rajesh Mondal <92754594+Developer-Rajesh100@users.noreply.github.com>
2024-09-07 23:42:30 +0530

    Django Module - 13.2 done

--- Software Development Project/Others/Django/forms_api/project/myapp/urls.py
+++ Software Development Project/Others/Django/forms_api/project/myapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('form/', views.register_form, name='form'),
     path('about/', views.about, name='about'),
+    path('django_form/', views.djangoForm, name='django_form'),
 ]
--- Software Development Project/Others/Django/forms_api/project/myapp/views.py
+++ Software Development Project/Others/Django/forms_api/project/myapp/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
+from .forms import contactForm
 
 # Create your views here.
 def home(request):
     return render(request, 'myapp/home.html')
 
 def about(request):
+    print(request.POST)
     name = request.POST.get('username')
     email = request.POST.get('email')
+    select = request.POST.get('select')
     if request.method == 'POST':
-        return render(request, 'myapp/about.html', {'name': name, 'email': email})
+        return render(request, 'myapp/about.html', {'name': name, 'email': email, 'select': select})
     else:
         return render(request, 'myapp/about.html')
 
 def register_form(request):
-
         return render(request, 'myapp/form.html')
+
+def djangoForm(request):
+    form = contactForm()
+    return render(request, 'myapp/django_form.html', {'form': form})
